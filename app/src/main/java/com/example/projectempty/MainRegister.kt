725328e411e9536ab8895412a726ab084734fbb9
akkcs <Aekkarat.c@kkumail.com>
2024-02-09 01:53:44 +0700

    test

---
+++ app/src/main/java/com/example/projectempty/MainRegister.kt
@@ -0,0 +1,11 @@
+package com.example.projectempty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainRegister : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_register)
+    }
+}
