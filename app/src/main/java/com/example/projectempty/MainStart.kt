c8888afeb0827bd48e71f4a7e7e4090668731f3f
akkcs <Aekkarat.c@kkumail.com>
2024-02-09 01:53:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projectempty/MainStart.kt
@@ -0,0 +1,25 @@
+package com.example.projectempty
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainStart : AppCompatActivity() {
+    var main_button_login: Button? = null
+    var main_button_create: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_start)
+        init()
+        main_button_login?.setOnClickListener {
+            var intent = Intent(this,MainLogin::class.java)
+            startActivity(intent)
+        }
+    }
+    fun init(){
+        main_button_login = findViewById(R.id.main_button_login)
+        main_button_create = findViewById(R.id.main_button_create)
+    }
+}
