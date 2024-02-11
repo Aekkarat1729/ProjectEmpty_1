590008059c504a7694c0acd935626ee1a54037d3
akkcs <Aekkarat.c@kkumail.com>
2024-02-12 03:08:56 +0700

    333333

---
+++ app/src/main/java/com/example/projectempty/ui/home/MphotoAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.projectempty.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projectempty.MphotoModel
+import com.example.projectempty.R
+import com.squareup.picasso.Picasso
+
+class MphotoAdapter (val MphotoList:List<MphotoModel>):RecyclerView.Adapter<ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.itemhorizontal, parent,false))
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val dataModel = MphotoList[position]
+        holder.textTitleItem.text = dataModel.title
+        Picasso.get().load(dataModel.Image)
+            .error(R.drawable.placeholder)
+            .placeholder(R.drawable.placeholder)
+            .into(holder.imageView)
+    }
+
+    override fun getItemCount(): Int {
+        return MphotoList.size
+    }
+
+
+}
---
+++ app/src/main/java/com/example/projectempty/ui/home/ViewHolder.kt
@@ -0,0 +1,12 @@
+package com.example.projectempty.ui.home
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projectempty.R
+
+class ViewHolder (View: View) : RecyclerView.ViewHolder(View){
+    var textTitleItem: TextView = View.findViewById(R.id.textTitleItem)
+    var imageView: ImageView = View.findViewById(R.id.imageView)
+}
