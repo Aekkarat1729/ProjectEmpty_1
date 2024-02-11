5150823624da961909a0d8d7ea1bed524811c419
PPortler <phitakpong2546@gmail.com>
2024-02-11 22:30:17 +0700

    login(email)

--- app/src/main/java/com/example/projectempty/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/projectempty/ui/home/HomeFragment.kt
@@ -7,7 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.projectempty.R
 import com.example.projectempty.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
 
@@ -15,8 +20,9 @@ class HomeFragment : Fragment() {
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
 
+    var mAuth: FirebaseAuth? = null
+    var home_text_wellcome:TextView? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,15 +31,16 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth!!.currentUser
+        home_text_wellcome = view.findViewById<TextView>(R.id.home_text_wellcome)
+        home_text_wellcome?.setText("Wellcome "+user?.email+" !!")
+
+        return view
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
