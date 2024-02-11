8981fc4c433ad0f45268d291bcf1d2673bde52ec
akkcs <Aekkarat.c@kkumail.com>
2024-02-12 03:54:48 +0700

    Home Fragment verion 189

--- app/src/main/java/com/example/projectempty/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/projectempty/ui/home/HomeFragment.kt
@@ -13,13 +13,17 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.projectempty.MphotoModel
 import com.example.projectempty.R
 import com.example.projectempty.databinding.FragmentHomeBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
@@ -28,10 +32,14 @@ import com.google.firebase.ktx.Firebase
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-    lateinit var RecyclerViewMphoto: RecyclerView
     var mAuth: FirebaseAuth? = null
     var home_text_wellcome: TextView? = null
     val myref = Firebase.database.reference
+    lateinit var RecyclerViewMphoto:RecyclerView
+    lateinit var database: FirebaseDatabase
+    lateinit var databaseReferenceMphoto: DatabaseReference
+    lateinit var responseMphoto:MutableList<MphotoModel>
+    private var MphotoAdapter: MphotoAdapter? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -40,6 +48,9 @@ class HomeFragment : Fragment() {
         val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         mAuth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+
+
         val user = mAuth!!.currentUser
         home_text_wellcome = view.findViewById<TextView>(R.id.home_text_wellcome)
 
@@ -72,14 +83,54 @@ class HomeFragment : Fragment() {
             .child(newMail)
             .child("Full name").addValueEventListener(postListener)
 
+
+
+
+        RecyclerViewMphoto.layoutManager = LinearLayoutManager(context , LinearLayoutManager.HORIZONTAL, false)
+        databaseReferenceMphoto = database.getReference("Mphoto/")
+        responseMphoto = mutableListOf()
+        MphotoAdapter = MphotoAdapter(responseMphoto as ArrayList<MphotoModel>)
+        RecyclerViewMphoto.adapter = MphotoAdapter
+        onBindingFirebase()
+
+
+
+
         return view
     }
 
+    private fun onBindingFirebase() {
+        databaseReferenceMphoto.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                responseMphoto.add(snapshot.getValue(MphotoModel::class.java)!!)
+                MphotoAdapter!!.notifyDataSetChanged()
+            }
 
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
+    }
 
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-}
+        companion object {
+            private const val TAG = "HomeFragment"
+        }
+    }
